6283b08dc32e4ace88be310c94f01592d2b8632e
Jph-y <josephllacuashh@gmail.com>
2025-06-16 15:44:31 -0500

    cambios variados

---
+++ js/flot/Utils/utils.js
@@ -0,0 +1,13 @@
+export const getMarca = () => document.querySelector('#selectMarca').value;
+export const getCantidad = () => document.querySelector('#cantidad').value;
+
+export function validarDatos(marca, cantidad) {
+    if (!marca || isNaN(cantidad) || cantidad <= 0) {
+        alert('Debe seleccionar una marca válida y una cantidad mayor a cero');
+        return false;
+    }
+    return true;
+}
+
+export const formatoMoneda = valor =>
+    valor.toLocaleString('es-PE', { style: 'currency', currency: 'PEN' });
---
+++ js/flot/trash/trash.js
@@ -0,0 +1,3 @@
+document.querySelector('#subtotal').textContent = formatoMoneda(subtotal);
+document.querySelector('#igv').textContent = formatoMoneda(igv);
+document.querySelector('#total').textContent = formatoMoneda(totalNeto);
