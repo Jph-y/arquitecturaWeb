9279d4ea11dfa22d15e498f6524aa5094e7a3d56
Jph-y <josephllacuashh@gmail.com>
2025-06-19 00:12:15 -0500

    update

--- js/promedio/promedio.js
+++ js/promedio/promedio.js
@@ -1,5 +1,6 @@
 import { getNombre, getApellido, getNota1, getNota2, getNota3, getNota4 } from "./utils.js";
 import { Promedio } from "./classPromedio.js";
+import { mostrarLista } from "./tblRegistro.js";
 
 let aEstudiantes = []
 let estudiantes
@@ -10,7 +11,6 @@ document.querySelector('#frmRegistro').addEventListener('submit', (e) => {
     const objPromedio = new Promedio(getNombre(), getApellido(), getNota1(), getNota2(), getNota3(), getNota4())
 
     var promedio = objPromedio.calcularPromedio();
-    alert(promedio)
 
     estudiantes = {
         nom_est: objPromedio.getNombre,
@@ -23,40 +23,14 @@ document.querySelector('#frmRegistro').addEventListener('submit', (e) => {
 
     aEstudiantes.push(estudiantes)
 
+    // var promedio = aEstudiantes.map(est =>{
+    //     return (parseFloat(est.n1_est) + parseFloat(est.n2_est) + parseFloat(est.n3_est) + parseFloat(est.n4_est)) / 4;
+    // })
+
     mostrarLista(aEstudiantes,promedio)
 })
 
-function mostrarLista(aEstudiantes,promedio) {
-    let modeloTabla = '';
-    modeloTabla += '<table class="table table-hover">';
-    modeloTabla += '<thead>';
-    modeloTabla += '<tr>';
-    modeloTabla += '<th>Nombre</th>';
-    modeloTabla += '<th>Apellido</th>';
-    modeloTabla += '<th>Nota 1</th>';
-    modeloTabla += '<th>Nota 2</th>';
-    modeloTabla += '<th>Nota 3</th>';
-    modeloTabla += '<th>Nota 4</th>';
-    modeloTabla += '<th>Promedio</th>';
-    modeloTabla += '</tr>';
-    modeloTabla += '</thead>';
-    aEstudiantes.forEach(est => {
-        modeloTabla += '<tbody>';
-        modeloTabla += '<tr>';
-        modeloTabla += '<td>' + est.nom_est + '</td>';
-        modeloTabla += '<td>' + est.ape_est + '</td>';
-        modeloTabla += '<td>' + est.n1_est + '</td>';
-        modeloTabla += '<td>' + est.n2_est + '</td>';
-        modeloTabla += '<td>' + est.n3_est + '</td>';
-        modeloTabla += '<td>' + est.n4_est + '</td>';
-        modeloTabla += '<td>' + promedio + '</td>';
-        modeloTabla += '</tr>';
-        modeloTabla += '</tbody>';
-    });
-    modeloTabla += '</table>';
 
-    document.querySelector('#tblRegistro').innerHTML = modeloTabla
-}
 
 
 
